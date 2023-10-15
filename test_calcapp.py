8c368f81fbfaee497e94ed451bf939ce0c1737b8
alexwajn <alewajn@gmail.com>
2023-10-15 11:53:17 -0500

    test complete, Dockerfile using python slim, changes to index2.html

--- test_calcapp.py
+++ test_calcapp.py
@@ -1,21 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
+import pytest
 
-driver = webdriver.Firefox()
-driver.get("http://localhost:5000")
-elem = driver.find_element(By.NAME, "a")
-elem.send_keys("4321")
+def get_result(a, b):
 
-elem = driver.find_element(By.NAME, "b")
-elem.send_keys("1234")
+    driver = webdriver.Firefox()
+    driver.get("http://localhost:5000")
+    
+    time.sleep(2)
+    
+    elem = driver.find_element(By.NAME, "a")
+    elem.send_keys(a)
+    
+    elem = driver.find_element(By.NAME, "b")
+    elem.send_keys(b)
+    
+    elem = driver.find_element(By.ID, "subs")
+    elem.click()
+    
+    elem.send_keys(Keys.RETURN)
+    
+    time.sleep(2)
 
-elem = driver.find_element(By.ID, "subs")
-elem.click()
+    elem = driver.find_element(By.NAME, "result")
+    result = elem.get_attribute('innerHTML')
+    driver.close()
+    return result
 
-elem.send_keys(Keys.RETURN)
+def test_res():
+    assert "3087" in get_result("4321", "1234")
 
-elem = driver.find_element(By.NAME, "result")
-print(elem.get_attribute('innerHTML'))
-
-driver.close()
